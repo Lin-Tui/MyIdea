ac0d61e814776bc4e1cbb1a5442207cc7e6d9bd0
lineve <lineve@163.com>
2022-04-06 23:38:49 +0800

    feat: init project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,53 @@
+import React from 'react';
+import { Counter } from './features/counter/Counter';
+
 function App() {
     return (
-        <div className="card w-96 bg-base-100 shadow-xl">
-            <figure>
-                <img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" />
-            </figure>
-            <div className="card-body">
-                <h2 className="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
-                </div>
-            </div>
+        <div className="App">
+            <header className="App-header">
+                <Counter />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <span>
+                    <span>Learn </span>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        React
+                    </a>
+                    <span>, </span>
+                    <a
+                        className="App-link"
+                        href="https://redux.js.org/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Redux
+                    </a>
+                    <span>, </span>
+                    <a
+                        className="App-link"
+                        href="https://redux-toolkit.js.org/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Redux Toolkit
+                    </a>
+                    ,<span> and </span>
+                    <a
+                        className="App-link"
+                        href="https://react-redux.js.org/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        React Redux
+                    </a>
+                </span>
+            </header>
         </div>
     );
 }
